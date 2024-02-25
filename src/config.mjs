ec4d26e1e935862ea67d318a5c1b8c3101111bce
Javier Guerrero <javigh87@gmail.com>
2024-02-25 10:17:26 +0100

    docs: config docs added

--- src/config.mjs
+++ src/config.mjs
@@ -1,51 +1,127 @@
+/**
+ * Configuración del canvas
+ *
+ * @typedef {Object} GameConfig
+ * @property {number} width - Ancho del canvas
+ * @property {number} height - Alto del canvas
+ */
+
+/**
+ * Configuración de los Sprite
+ *
+ * @typedef {Object} SpriteConfig
+ * @property {number} xStart - Posición x inicial del sprite
+ * @property {number} yStart - Posición y inicial del sprite
+ * @property {number} width - Ancho del sprite
+ * @property {number} height - Alto del sprite
+ */
+
+/**
+ * Configuración de la pelota
+ *
+ * @typedef {Object} BallConfig
+ * @property {number} width - Ancho de la pelota
+ * @property {number} height - Alto de la pelota
+ * @property {number} speedX - Velocidad en el eje X de la pelota
+ * @property {number} speedY - Velocidad en el eje Y de la pelota
+ * @property {SpriteConfig} sprite
+ */
+
+/**
+ * Configuración del listado de bloques
+ * 
+ * @typedef {Object} BricksConfig
+ * @property {number} rowCount - Numero de filas
+ * @property {number} columnCount - Numero de columnas
+ * @property {number} offsetTop - Padding superior
+ * @property {number} offsetLeft - Padding izquierdo
+ */
+
+/**
+ * Configuración de los bloques
+ * 
+ * @typedef {Object} BrickConfig
+ * @property {number} width - Ancho del bloque
+ * @property {number} height - Alto del bloque
+ */
+
+/**
+ * Configuración del paddle
+ * 
+ * @typedef {Object} PaddleConfig
+ * @property {number} width - Ancho del paddle
+ * @property {number} height - Alto del paddle
+ * @property {number} sensitivity - Sensibilidad del movimiento del paddle
+ * @property {SpriteConfig} sprite
+ */
+
 /* -- GAME -- */
+/**
+ * @type {GameConfig}
+ */
 export const GAME_CONFIG = {
   width: 448,
   height: 400,
 };
 
 /* -- BALL -- */
+/**
+ * @type {BallConfig}
+ */
 export const BALL_CONFIG = {
-  radius: 3,
+  width: 8,
+  height: 8,
   speedX: 3,
   speedY: -3,
   sprite: {
-    spriteXStart: 0,
-    spriteYStart: 0,
-    spriteWidth: 22,
-    spriteHeight: 22,
+    xStart: 0,
+    yStart: 0,
+    width: 22,
+    height: 22,
   },
-  height: 8,
-  width: 8,
 };
 
 /* -- BRICKS -- */
+/**
+ * @type {BricksConfig}
+ */
 export const BRICKS_CONFIG = {
   rowCount: 6,
   columnCount: 13,
   offsetTop: 80,
   offsetLeft: 16,
-  padding: 0,
 };
 
+/**
+ * @type {BrickConfig}
+ */
 export const BRICK_CONFIG = {
   width: 32,
   height: 16,
 };
 
+/**
+ * Estados de los bloques
+ *
+ * @enum {number}
+ */
 export const BRICK_STATUS = {
   ACTIVE: 1,
   DESTROYED: 0,
 };
 
+/* -- PADDLE -- */
+/**
+ * @type {PaddleConfig}
+ */
 export const PADDLE_CONFIG = {
   width: 50,
   height: 10,
   sensitivity: 7,
   sprite: {
-    spriteXStart: 0,
-    spriteYStart: 0,
-    spriteWidth: 104,
-    spriteHeight: 24,
+    xStart: 0,
+    yStart: 0,
+    width: 104,
+    height: 24,
   },
 };
