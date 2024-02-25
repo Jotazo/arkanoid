40316c6a20b62639ebc340ef7c662fea0d52d884
Javier Guerrero <javigh87@gmail.com>
2024-02-25 10:45:18 +0100

    feature: removed old sprites. Added new sprites & imports

---
+++ src/assets.mjs
@@ -0,0 +1,17 @@
+/* BALLS ASSETS */
+const ballBlue = new Image();
+ballBlue.src = "../assets/balls/ballBlue.png";
+const ballGrey = new Image();
+ballGrey.src = "../assets/balls/ballGrey.png";
+
+/* PADDLES ASSETS */
+const paddleRed = new Image();
+paddleRed.src = "../assets/paddles/paddleRed.png";
+const paddleBlue = new Image();
+paddleBlue.src = "../assets/paddles/paddleBlue.png";
+
+/* BRICKS ASSETS */
+const redBrick = new Image();
+redBrick.src = "../assets/bricks/redBrick.png";
+
+export { ballBlue, ballGrey, paddleBlue, paddleRed, redBrick };
